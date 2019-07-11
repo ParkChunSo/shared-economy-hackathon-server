f94fd3f7cadc0cd319c1ccda61f135664cb8e6fc
ParkChunSo <cnsth123@gmail.com>
2019-07-11 17:15:25 +0900

    Interceptor를 사용하여 Jwt 처리

---
+++ src/main/java/com/hackathon/sharedeconomy/interceptor/AuthenticationInterceptHandler.java
@@ -0,0 +1,40 @@
+package com.hackathon.sharedeconomy.interceptor;
+
+import com.hackathon.sharedeconomy.exception.AuthenticationException;
+import com.hackathon.sharedeconomy.model.enums.RoleType;
+import com.hackathon.sharedeconomy.utill.JwtProvider;
+import io.jsonwebtoken.JwtException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@Slf4j
+public class AuthenticationInterceptHandler extends HandlerInterceptorAdapter {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("================== 토큰 검사 실행 ==================");
+        try {
+            String token = JwtProvider.resolveToken(request);
+            if (token != null && JwtProvider.validateToken(token)) {
+                RoleType roles = JwtProvider.getUserRoleByToken(token);
+                if(!roles.equals(RoleType.ADMIN))
+                    throw new AuthenticationException("해당 요청은 관리자만 가능합니다.");
+            }
+        } catch (JwtException | IllegalArgumentException e){
+            log.error("Expired or invalid JWT token");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Expired or invalid JWT token");
+            return false;
+        }
+//        catch (AuthenticationException e){
+//            log.error("User not found");
+//            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//            response.getWriter().write("User not found");
+//            return false;
+//        }
+        return true;
+    }
+}
